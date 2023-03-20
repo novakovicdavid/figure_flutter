8c0292831b249d6e5a9bfdf7e38aa36f06f9963d
David <83370778+novakovicdavid@users.noreply.github.com>
2023-03-20 22:34:28 +0100

    Responsive signup

--- lib/signup.dart
+++ lib/signup.dart
@@ -16,20 +16,25 @@ class SignupForm extends StatefulWidget {
 
 class SignupFormState extends State<SignupForm> {
   final _formKey = GlobalKey<FormState>();
-  static RegExp emailRegex = RegExp("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}");
+  static RegExp emailRegex = RegExp(
+      "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}");
   static RegExp usernameRegex = RegExp("^[a-zA-Z0-9]+-*[a-zA-Z0-9]+?");
   var usernameValidator = (String? username) {
     if (username == null || username.isEmpty) return 'Please enter a username';
-    if (username.length < 3) return 'Username must be at least 3 characters long';
-    if (!usernameRegex.hasMatch(username)) return "Username is not valid. Only the characters A-Z, a-z and '-' are accepted.";
+    if (username.length < 3)
+      return 'Username must be at least 3 characters long';
+    if (!usernameRegex.hasMatch(username))
+      return "Username is not valid. Only the characters A-Z, a-z and '-' are accepted.";
     return null;
   };
   var emailValidator = (String? email) {
-    if (email == null || !emailRegex.hasMatch(email)) return "Please enter a valid email";
+    if (email == null || !emailRegex.hasMatch(email))
+      return "Please enter a valid email";
     return null;
   };
   var passwordValidator = (String? password) {
-    if (password == null || password.length < 6) return "Password must be >= 6 characters long";
+    if (password == null || password.length < 6)
+      return "Password must be >= 6 characters long";
     return null;
   };
 
@@ -39,46 +44,80 @@ class SignupFormState extends State<SignupForm> {
 
   @override
   Widget build(BuildContext context) {
-    return (Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              validator: (value) => usernameValidator(value),
-              onChanged: (value) => username = value,
-            ),
-            TextFormField(
-              validator: (value) => emailValidator(value),
-              onChanged: (value) => email = value,
-            ),
-            TextFormField(
-              validator: (value) => passwordValidator(value),
-              onChanged: (value) => password = value,
-              obscureText: true,
-            ),
-            ElevatedButton(onPressed: () async {
-              if (_formKey.currentState!.validate()) {
-                var data = {
-                  "username": username,
-                  "email": email,
-                  "password": password
-                };
-                var json = jsonEncode(data);
-                var connection = await httpclient.postUrl(Uri.parse("https://backend.figure.novakovic.be/users/signup"));
-                connection.headers.add("content-type", "application/json");
-                connection.headers.contentLength = json.length;
-                connection.write(json);
-                var response = await connection.close();
-                var parsedResponseBody = jsonDecode(await readResponse(response));
-                if (parsedResponseBody["profile"] != null) {
-                  setState(() {
-                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const BrowsePage()), (route) => false,);
-                  });
-                }
-              }
-            }, child: const Text("Sign up!"))
-          ],
-        )));
+    var title = Text(
+        style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 3.0),
+        "Join Figure.");
+
+    return (Scaffold(body: Column(
+      children: [
+        Expanded(
+            child: Center(
+                child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Container(
+                        margin: const EdgeInsets.only(left: 40, right: 40),
+                        child: title)))),
+        Container(margin: const EdgeInsets.only(left: 30, right: 30, bottom: 60), child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) => usernameValidator(value),
+                  onChanged: (value) => username = value,
+                  decoration: const InputDecoration(
+                    labelText: "Username"
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) => emailValidator(value),
+                  onChanged: (value) => email = value,
+                  decoration: const InputDecoration(
+                      labelText: "Email"
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) => passwordValidator(value),
+                  onChanged: (value) => password = value,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      labelText: "Password"
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        var data = {
+                          "username": username,
+                          "email": email,
+                          "password": password
+                        };
+                        var json = jsonEncode(data);
+                        var connection = await httpclient.postUrl(Uri.parse(
+                            "https://backend.figure.novakovic.be/users/signup"));
+                        connection.headers
+                            .add("content-type", "application/json");
+                        connection.headers.contentLength = json.length;
+                        connection.write(json);
+                        var response = await connection.close();
+                        var parsedResponseBody =
+                        jsonDecode(await readResponse(response));
+                        if (parsedResponseBody["profile"] != null) {
+                          setState(() {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const BrowsePage()),
+                                  (route) => false,
+                            );
+                          });
+                        }
+                      }
+                    },
+                    child: const Text("Sign up"))
+              ],
+            )))
+      ],
+    )));
   }
 }
 
